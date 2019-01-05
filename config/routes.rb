ae96a36d44a6100a5cf5c5298e5c7ff496e61cbd
Cezik <czepek221@gmail.com>
2019-01-05 15:04:05 +0100

    Add comment model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # get '/', to: 'welcome#index'
   root to: 'articles#index'
 
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
