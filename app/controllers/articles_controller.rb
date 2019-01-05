09dc43a2a71c4955251564319a4b780fe42e2438
Cezik <czepek221@gmail.com>
2019-01-05 09:14:09 +0100

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+    render
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = t('articles.create.success')
+      redirect_to article_path(@article.id)
+    else
+      flash.now[:error] = t('articles.create.error',
+      problems: @article.errors.full_messages.to_sentence
+      )
+      render 'new' # z views/articles/new.html.rb
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    # render
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      # redirect_to article_path(@article.id)
+      flash[:success] = t('articles.update.success')
+      redirect_to @article
+    else
+      flash.now[:error] = t('articles.update.error',
+      problems: @article.errors.full_messages.to_sentence
+      )
+      render 'edit' # TODO: Poinformować,co się nie udało
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
