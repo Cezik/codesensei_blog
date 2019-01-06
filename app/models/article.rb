93797178c4f43c0f8b5ff00d8ab91b1aac53df04
Cezik <czepek221@gmail.com>
2019-01-06 14:58:55 +0100

    Added missed translation and terms of service validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,9 @@ class Article < ApplicationRecord
   validates :title, :text, :author, presence: true
   validates :title, length: { minimum: 5, maximum: 40 }
   validates :title, uniqueness: true, if: :title_changed?
+  validates :terms_of_service, acceptance: true
 
-  validate :custom_validation, if: :text 
+  validate :custom_validation, if: :text
 
   def custom_validation
     if text.index(title)
