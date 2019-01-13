31a6cea936892f8a9e44f4ca93f827c0b8dd57d1
Cezik <czepek221@gmail.com>
2019-01-13 12:40:46 +0100

    Added polymporphic association between comments and other models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_151835) do
+ActiveRecord::Schema.define(version: 2019_01_13_103024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,12 @@ ActiveRecord::Schema.define(version: 2019_01_12_151835) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter", null: false
     t.text "body", null: false
-    t.bigint "article_id", null: false
+    t.bigint "commentable_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.string "commentable_type", default: "Article", null: false
+    t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -57,6 +59,5 @@ ActiveRecord::Schema.define(version: 2019_01_12_151835) do
   end
 
   add_foreign_key "articles", "users"
-  add_foreign_key "comments", "articles"
   add_foreign_key "pictures", "articles"
 end
